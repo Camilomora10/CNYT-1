a69f9484c3b70ff8defda009adfcfba75b60a255
unknown <ivan.rincon-s@labinfo.is.escuelaing.edu.co>
2020-03-12 15:40:31 -0500

    terminar

--- Implementations/teoriaCuantica/teoriaCuantica.py
+++ Implementations/teoriaCuantica/teoriaCuantica.py
@@ -26,24 +26,27 @@ def bra( vect  ):
 def transicion( vect1 ,vect2):
     asnw = internalProduct( bra(vect1 ), vect2)
     
-    return asnw 
+    return asnw
+
+
+def probability( vector, position ):
+    Norma = norma( vector)
+    if ( 0<= position < len( vector) ):
+        print( ((module(vector[position])**2) / Norma ) *100)
+    
+    
+    
 def main():
     vect1 = normalizate( [[2, 1], [-1, 2], [0, 1], [1, 0], [3, -1], [2, 0], [0, -2], [-2, 1], [1, -3], [0, -1]] )
 
     vect2 =normalizate( [[-1, -4], [2, -3], [-7, 6], [-1, 1], [-5, -3], [5, 0], [5, 8], [4, -4], [8, -7], [2, -7]] )
     
-    print(  transicion(  vect2   ,vect1) ) 
+    #print(  transicion(  vect2   ,vect1) )
+    #probability(vect1,7 );
+    
 main()
 
 
 
 
-def main():
-    x0 = [ [0,0] for x in range( 10 )]
-    x1 =x0[:]
-
-    x0[0] = [1,0]; x1[1] = [1,0]
-    print(  transicion( span( x0 )  ,x1) )
-    
-    
 
